f9e99d402c8f38dbf84e32eb3eed0fabb1a5f248
Raghvendra Singh <Raghav26897@gmail.com>
2022-01-05 01:00:28 +0530

    updated

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,60 +1,62 @@
-const users = []
+const users = [];
 
 //addUser
 
-const addUser = ({id,username,room}) => {
-    // username = username.trim().toLowerCase()
-    // room = room.trim().toLowerCase()
+const addUser = ({ id, username, room }) => {
+  // username = username.trim().toLowerCase()
+  // room = room.trim().toLowerCase()
 
-    //validate Data
-    if(!username || !room) {
-        return {
-            error: 'Username and Room are required'
-        }
-    }
-
-    //Check for existing user
-    const existingUser  = users.find((user)=>{
-        return user.room === room && user.username === username
-    })
-
-    //Validate username
-    if(existingUser) {
-        return {
-            error: "Username already taken!"
-        }
-    }
-
-    //Store User
-    const user = {id, username, room}
-    users.push(user)
+  //validate Data
+  if (!username || !room) {
     return {
-        user
-    }
-}
+      error: "Username and Room are required",
+    };
+  }
+
+  //Check for existing user
+  const existingUser = users.find((user) => {
+    return user.room === room && user.username === username;
+  });
+
+  //Validate username
+  if (existingUser) {
+    return {
+      error: "Username already taken!",
+    };
+  }
+
+  //Store User
+  const user = { id, username, room };
+  users.push(user);
+  return {
+    user,
+  };
+};
 
 //removeUser
 const removeUser = (id) => {
-    const index = users.findIndex((user)=>user.id===id)
-    if(index !== -1) {
-        return users.splice(index,1)[0]
-    }
-}
+  const index = users.findIndex((user) => user.id === id);
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
 
 //getUser
 const getUser = (id) => {
-    return users.find((user)=>user.id === id)
-}
+  return users.find((user) => user.id === id);
+};
 
 //getUsersInRoom
 const getUsersInRoom = (room) => {
-    // room = room.trim().toLowerCase()
-    return users.filter((user)=>user.room === room)
-}
-console.log(users)
+  // room = room.trim().toLowerCase()
+  return users.filter((user) => user.room === room);
+};
+
+console.log(users);
+
 module.exports = {
-    addUser,
-    getUser,
-    getUsersInRoom,
-    removeUser
-}
+  addUser,
+  getUser,
+  getUsersInRoom,
+  removeUser,
+};
